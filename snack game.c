46f171e22819420b362d2ad3124f2ec56c166d21
Annapurna Gupta <ganu48600@gmail.com>
2024-05-11 20:11:11 +0530

    Add files via upload
    Project Title: Snake Game in C
    
    Description:
    This project is a classic Snake game implemented in the C programming language. Players control a snake that moves around the game board, eating food represented by '@' symbols to grow longer. The game ends when the snake collides with the boundaries of the board or with itself.
    
    Features:
    Simple yet addictive gameplay.
    Smooth snake movement and control.
    Randomly generated food for the snake to eat.
    Dynamic scoring system that increases as the snake eats food and grows longer.
    Game over detection when the snake collides with the boundaries or itself.
    
    How to Play:
    Use the 'W', 'A', 'S', 'D' keys to control the snake's movement (up, left, down, right respectively).
    Navigate the snake to eat the '@' symbols to grow longer.
    Avoid colliding with the boundaries or the snake's own body.
    The game ends when the snake collides, and your score is displayed.
    
    Installation:
    Clone the repository to your local machine.
    Compile the source code using a C compiler (e.g., GCC).
    Run the compiled executable to start the game.
    Contributing:
    Contributions to this project are welcome! Feel free to fork the repository, make improvements or fixes, and submit pull requests.
    
    
    
    Credits:
    Developed by Annapurna Gupta.
    Inspired by classic Snake games.
    
    Contact:
    For any questions or feedback, please contact ganu48600@gmail.com .
    
    Feel free to customize this description further with any additional information specific to your project.

--- snack game.c
+++ snack game.c
@@ -2,72 +2,155 @@
 #include<stdlib.h>
 #include<conio.h>
 
-void snack();
-void body();
-void printsn();
-int i ,j ,height=20 , width=40;
-int x,y,obx,oby,SNACK;
+void setup();
+void draw();
+void input();
+void mainlogic();
+int tailx[100], taily[100];
+int piece=0;
+int i ,j ,height=30 , width=50;
+int x,y,obx,oby,flag,gameend,score;
 
 int main(){
-	snack();
-	body();
+	setup();
+	while (gameend != 1) {
+		input();
+		draw();
+		mainlogic();
+		
+		usleep(1000);
+	
+	}
+	
 }
 
-void printsn(){
+void mainlogic(){
 	
-	if (kbhit){
-		switch( getch()){
-			case 'W' :
-			 SNACK = 1 ;
-			 break;
+	int i , x1 ,y1 ,x2 ,y2;
+	x1 = tailx[0];
+	y1 = taily[0];
+	tailx[0]=x;
+	taily[0]=y;
+	for ( i = 1; i <= piece ; i++){
+		x2=tailx[i];
+		y2=taily[i];
+		tailx[i]=x1;
+		taily[i]=y1;
+		x1=x2;
+		y1=y2;
+	}
+	switch(flag){
+		case 1: {
+			x--;
+			break;
 		}
-		switch( getch()){
-			case 'S' :
-			 SNACK = 2 ;
-			 break;
+		case 2: {
+			x++;
+			break;
 		}
-		switch( getch()){
-			case 'A' :
-			 SNACK = 3 ;
-			 break;
+		case 3: {
+			y--;
+			break;
 		}
-		switch( getch()){
-			case 'D' :
-			 SNACK = 4 ;
-			 break;
+		case 4: {
+			y++;
+			break;
 		}
+	}
+	if ( x==0 || x==height-1 || y == 0 || y == width-1){
+		gameend = 1;
+	}
+	if( x == obx && y == oby ){
 		
+	label3:
+		obx=rand()%30;
+		if( obx == 0 )
+		goto label3;
+		
+		label4:
+		oby=rand()%50;
+		if( oby == 0 )
+		goto label4;
+		
+		piece++;
 	}
 }
-void snack(){
+
+void input(){
+	
+	if (kbhit()){
+		 switch(getch()){
+		 	
+		 	case 'w': {
+		 		flag = 1;
+				break;
+			 }
+		 
+		 	case 's': {
+		 		flag = 2;
+				break;
+			 }
+		 	
+		 	case 'a': {
+		 		flag = 3;
+				break;
+			 }
+		 	
+		 	
+		 	case 'd': {
+		 		flag = 4;
+				break;
+			 }
+		 	
+		 
+}
+}
+}
+void setup(){
 	x = height/2;
 	y = width/2;
 	
-	do{
-		obx = rand()%20;
-	}while( obx == 0 );
-	do{
-		oby = rand()%40;
-	}while( oby == 0);
+	label1:
+		obx=rand()%30;
+		if( obx == 0 )
+		goto label1;
+		
+		label2:
+		oby=rand()%50;
+		if( oby == 0 )
+		goto label2;
+		
+	gameend = 0;
+	score = 0;
+	piece = 0;
 }
 
-void body(){
+void draw(){
+	int k;
+	system("cls");
 	for( i=0 ; i < height ; i++){
 		for( j=0 ; j < width; j++){
 			if( i == 0 || i == height - 1 || j == 0 || j == width-1){
 			printf("*");
 		}else{
 			if( i == x && j == y){
-				printf("00");
+				printf("0");
 			}
 			else if ( i == obx  && j == oby ){
 				printf("@");
 			}else{
+				for( k=0 ; k<piece ; k++){
+					if( i == tailx[k] && j == taily[k]){
+						printf("o");
+					}
+				}
 				printf(" ");
 			}	
 		}
 		}
 		printf("\n");
 	}
-	
 }
+
+
+
